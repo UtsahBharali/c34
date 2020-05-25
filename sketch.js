3f1171f76bf282273d95adf929ed42fb8e6ac61b
UtsahBharali <bharaliutsah@gmail.com>
2020-05-25 20:58:47 +0530

    c34

--- sketch.js
+++ sketch.js
@@ -1,29 +1,51 @@
 var ball;
+var database,position
 
 function setup(){
+    database = firebase.database();
+    console.log(database);
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+
+    var ballposition = database.ref('ball/position');
+     ballposition.on("value",readposition,showerorr)
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writeposition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writeposition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writeposition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writeposition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writeposition(x,y){
+    database.ref('ball/position').set({
+        'x': position.x + x,
+        'y': position.y + y
+    })
+    }
+
+function readposition(data){
+position = data.val();
+console.log(position.x);
+console.log(position.y);
+ball.x = position.x;
+ball.y = position.y;
+
 }
+
+function showerorr(){
+
+}
+
